e9e255d4b4192bfc9c55568b75574d15a2f4ee71
Xiaolong Huang <xiaolong_huang@foxmail.com>
2018-07-25 18:18:20 +1200

    init

---
+++ learn/Human.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace learn
+{
+    public class Human
+    {
+        
+        public string firstname;
+        private int age;
+
+        public Human(string name)
+        {
+            firstname = name;
+        }
+
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hi, I am "+firstname+" and I am "+age);
+        }
+                
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+    }
+}
---
+++ learn/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace learn
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int counter = 0;  
+            string line;  
+            Dictionary<string,int> map = new Dictionary<string, int>();
+
+            System.IO.StreamReader file = new System.IO.StreamReader(@"/Users/huangxiaolong/RiderProjects/learn/learn/config.txt");  
+            while((line = file.ReadLine()) != null)  
+            {  
+                //System.Console.WriteLine (line);  
+                counter++;
+                if (!line.StartsWith("//"))
+                {
+                    string[] bit = line.Split(' ');
+                    map.Add(bit[0],int.Parse(bit[1]));
+                }
+            }  
+
+            file.Close();  
+            Console.WriteLine("There were {0} lines.", counter);  
+            Console.WriteLine(map.Count);
+            // Suspend the screen.  
+            Console.ReadLine();  
+        }
+    }
+}
