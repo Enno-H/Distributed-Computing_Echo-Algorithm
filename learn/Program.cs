c6dda1c3782a6a7c5b7a95d222f808e6ea8e5d7e
Xiaolong Huang <xiaolong_huang@foxmail.com>
2018-07-26 11:31:15 +1200

    WCF host part1

--- learn/Program.cs
+++ learn/Program.cs
@@ -2,11 +2,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.ServiceModel.Description;
+using System.Threading;
 
 namespace learn
 {
-    internal class Program
+    
+    public class Program
     {
+        static public int ThisNode;
+        
         public static void Main(string[] args)
         {
             int counter = 0;  
@@ -34,60 +41,90 @@ namespace learn
             tt.Add(2);
             tt.Add(3);
             tt.Add(4);
+          
+            Node node = new Node(tt);
+            Console.WriteLine(node.Adj.Count);
             
-            Node node1 = new Node(tt);
             
-            Console.WriteLine(node1.Adj.Count);
+            
+            Uri baseAddress = new Uri("http://localhost:8081/hello");
 
-            
-            
-            
-            
-            // Suspend the screen.  
-            Console.ReadLine();  
-        }
+            WebServiceHost host = null;
 
-        public void readFile()
-        {
-            int counter = 0;  
-            string line;  
-            Dictionary<string,int> map = new Dictionary<string, int>();
+            try {
+                host = new WebServiceHost(typeof(HelloWorldService), baseAddress);
+                ServiceEndpoint ep = host.AddServiceEndpoint(typeof(INodeService), new WebHttpBinding(), "");
 
-            System.IO.StreamReader file = new System.IO.StreamReader(@"/Users/huangxiaolong/RiderProjects/learn/learn/config.txt");  
-            while((line = file.ReadLine()) != null)  
-            {  
-                //System.Console.WriteLine (line);  
-                counter++;
-                if (!line.StartsWith("//"))
-                {
-                    string[] bit = line.Split(' ');
-                    map.Add(bit[0],int.Parse(bit[1]));
+                host.Open();
+			
+                var names = Environment.GetCommandLineArgs();
+                foreach (var name in names){
+                    Console.WriteLine(name);
                 }
-            }  
 
-            file.Close();  
-            Console.WriteLine("There were {0} lines.", counter);  
-            Console.WriteLine(map.Count);
-            // Suspend the screen.  
-            Console.ReadLine();
+                Console.WriteLine($"The service is ready at {baseAddress}/Hello?name=xyz or /SayHello?name=xyz");
+                Console.WriteLine("Press <Enter> to stop the service.");
+                Console.ReadLine();
+
+                // Close the ServiceHost.
+                host.Close();
+        
+            } catch (Exception ex) {
+                Console.WriteLine($"*** Exception {ex.Message}");
+                host = null;
+        
+            } finally {
+                if (host != null) ((IDisposable)host).Dispose();
+            }
+            
         }
         
         
+        
+        [ServiceContract()]
+        public interface INodeService {
+            [OperationContract(IsOneWay=true)]
+            [WebGet()]
+            void Message(int from, int to, int tok, int pay);
+            
+            [OperationContract()] 
+            [WebGet(ResponseFormat=WebMessageFormat.Json)]
+            string SayHello(string name);
+        }
+        
+        [ServiceBehavior(
+            InstanceContextMode=InstanceContextMode.Single, 
+            ConcurrencyMode=ConcurrencyMode.Multiple)] 
+        public class HelloWorldService : INodeService {
+            public static Func<int> Tid = () => Thread.CurrentThread.ManagedThreadId;
+            
+            public static Func<double> Millis = () => DateTime.Now.TimeOfDay.TotalMilliseconds;
+            
+    
+            public void Message(int from, int to, int tok, int pay){
+                Console.WriteLine($"... {Millis():F2} {ThisNode} < {from} {to} {tok} {pay}");
+            }
+            
+            
+            public string SayHello(string name) {
+                Console.WriteLine($"[{Tid()}] [{Millis()}] SayHello received: {name}");
+                return $"Hello, {name}";
+            }
+        }
+    
         internal class Node
         {
             private int id;
             private Boolean visited;
             private ArrayList adj;
-            
-            /*
+                
             public Node()
             {
                 id = -1;
                 visited = false;
                 adj = new ArrayList();
             }
-            */
-
+    
             public Node(ArrayList al)
             {
                 if (al.Count < 2)
@@ -105,43 +142,23 @@ namespace learn
                 }
             }
             
-            
-            public int Id   
+            public int Id
             {
-                get
-                {
-                    return id;
-                }
-                set
-                {
-                    id = value;
-                }
+                get => id;
+                set => id = value;
             }
-            
-            public Boolean Visited   
+                
+            public Boolean Visited
             {
-                get
-                {
-                    return visited;
-                }
-                set
-                {
-                    visited = value;
-                }
+                get => visited;
+                set => visited = value;
             }
-            
-            public ArrayList Adj   
+                
+            public ArrayList Adj
             {
-                get
-                {
-                    return adj;
-                }
-                set
-                {
-                    adj = value;
-                }
-            }
-
-        }
+                get => adj;
+                set => adj = value;
+            }   
+        }   
     }
 }
