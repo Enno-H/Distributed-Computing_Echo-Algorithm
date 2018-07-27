ee116def09e71ec5f9a73a8e5a0e26f459fd7938
Xiaolong Huang <xiaolong_huang@foxmail.com>
2018-07-27 16:04:18 +1200

    adapted to run on windows

--- learn/Program.cs
+++ learn/Program.cs
@@ -10,26 +10,29 @@ using System.Threading;
 namespace learn
 {
     [ServiceContract()]
-    public interface INodeService {
-        [OperationContract(IsOneWay=true)]
+    public interface INodeService
+    {
+        [OperationContract(IsOneWay = true)]
         [WebGet()]
         void Message(int from, int to, int tok, int pay);
-            
-        [OperationContract()] 
-        [WebGet(ResponseFormat=WebMessageFormat.Json)]
+
+        [OperationContract()]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
         string SayHello(string name);
     }
-        
+
     [ServiceBehavior(
-        InstanceContextMode=InstanceContextMode.Single, 
-        ConcurrencyMode=ConcurrencyMode.Multiple)] 
-    public class HelloWorldService : INodeService {
+        InstanceContextMode = InstanceContextMode.Single,
+        ConcurrencyMode = ConcurrencyMode.Multiple)]
+    public class HelloWorldService : INodeService
+    {
         public static Func<int> Tid = () => Thread.CurrentThread.ManagedThreadId;
-            
+
         public static Func<double> Millis = () => DateTime.Now.TimeOfDay.TotalMilliseconds;
-            
-    
-        public void Message(int from, int to, int tok, int pay){
+
+
+        public void Message(int from, int to, int tok, int pay)
+        {
             Console.WriteLine($"... {Millis():F2} {Node.ThisNode} < {from} {to} {tok} {pay}");
             if (to == Node.ThisNode)
             {
@@ -37,15 +40,15 @@ namespace learn
                 {
                     Node.Parent = from;
                 }
-                    
-                    
-            }
 
+
+            }
         }
-              
-        public string SayHello(string name) {
-            Console.WriteLine($"[{Tid()}] [{Millis()}] SayHello received: {name}");
-            return $"Hello, {name}";
+
+        public string SayHello(string name)
+        {
+            Console.WriteLine($"[{Tid()}] [{Millis()}] {Node.ThisNode} received: {name}");
+            return $"{Node.ThisNode}: Hello, {name}";
         }
     }
     
@@ -56,158 +59,141 @@ namespace learn
         static public ArrayList Adj;
         static public Boolean Visited;
         static public int Parent;
-
+    
         static public Dictionary<string, int> map;
-        
+
         public static void Main(string[] args)
         {
-            
+    
             //Step 0: Get input
+            
+            /*
+            ArrayList inputList = new ArrayList();
             var inputs = Environment.GetCommandLineArgs();
             foreach (var input in inputs){
-                Console.WriteLine(input);
+                //Console.WriteLine(input);
+                inputList.Add(input);
             }
-
-            Console.WriteLine(inputs[0]);
+            inputList.RemoveAt(0);
+            inputList.RemoveAt(0);
+    
+            Console.WriteLine("The inputList length is " + inputList.Count+" and the first is "+inputList[0]);
+            */
             
-            
-            
-            
+            ArrayList inputList = new ArrayList();
+            inputList.Add(1);
+            inputList.Add(2);
+            inputList.Add(3);
+            inputList.Add(4);
+               
+    
+    
+    
+           
+    
+    
             //Step 1: Config.txt -> Map
             //Input 1
-            int counter = 0;  
-            string line;  
+            int counter = 0;
+            string line;
             map = new Dictionary<string, int>();
-
-            System.IO.StreamReader file = new System.IO.StreamReader(@"/Users/huangxiaolong/RiderProjects/learn/learn/config.txt");  
-            while((line = file.ReadLine()) != null)  
-            {  
+    
+            System.IO.StreamReader file = new System.IO.StreamReader(@"/Users/huangxiaolong/RiderProjects/learn/learn/config.txt");
+            while ((line = file.ReadLine()) != null)
+            {
                 //System.Console.WriteLine (line);  
                 counter++;
                 if (!line.StartsWith("//"))
                 {
                     string[] bit = line.Split(' ');
-                    map.Add(bit[0],int.Parse(bit[1]));
+                    map.Add(bit[0], int.Parse(bit[1]));
                 }
-            }  
-
-            file.Close();  
-            Console.WriteLine("There were {0} lines.", counter);  
-            Console.WriteLine(map.Count);
-            
-            /*
-            ArrayList tt = new ArrayList();
-            tt.Add(1);
-            tt.Add(2);
-            tt.Add(3);
-            tt.Add(4);
-          
-            Node node = new Node(tt);
-            Console.WriteLine(node.Adj.Count);
-            */
-            
+            }
+    
+            file.Close();
+            Console.WriteLine("There are {0} lines in Config.txt", counter);
+            Console.WriteLine("There are "+map.Count+" real data in the Config file");
+    
+    
             //Step 2: ArrayList -> Node 
             //Input 2
             Parent = -1;
-            ArrayList tt = new ArrayList();
-            tt.Add(1);
-            tt.Add(2);
-            tt.Add(3);
-            tt.Add(4);
-            
-            if (tt.Count < 2)
+           
+            if (inputList.Count < 2)
             {
                 Console.WriteLine("Wrong Input!!!");
             }
             else
             {
-                ThisNode = (int)tt[0];
+                //ThisNode = (int)inputList[0];
+                ThisNode = Convert.ToInt32(inputList[0]);
+    
                 Adj = new ArrayList();
-                for (int i = 1; i < tt.Count; i++)
+    
+                for (int i = 1; i < inputList.Count; i++)
                 {
-                    Adj.Add((int)tt[i]);
+                    Adj.Add(inputList[i]);
                 }
             }
-            
-            
-            
-            
-            
-            
-            
-            
-            
+    
+            Console.WriteLine("This Node is: " + ThisNode);
+            Console.WriteLine("It has Adj: " + Adj.Count + ", the first is " + Adj[0]);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
             //Step 3: Create Host
-            Uri baseAddress = new Uri("http://localhost:8081/hello");
-
+            Console.WriteLine($"Id: {ThisNode}, value: {map[ThisNode.ToString()]}");
+            String url = "http://localhost:"+map[ThisNode.ToString()] + "/hello";
+            Console.WriteLine(url);
+            Uri baseAddress = new Uri(url);
+    
             WebServiceHost host = null;
-
-            try {
-                
+    
+            try
+            {
+    
                 host = new WebServiceHost(typeof(HelloWorldService), baseAddress);
                 ServiceEndpoint ep = host.AddServiceEndpoint(typeof(INodeService), new WebHttpBinding(), "");
-
+    
                 host.Open();
-			
+    
                 //ClientClass.ClientTo();
-                
-                
+    
+    
                 Console.WriteLine($"The service is ready at {baseAddress}/Hello?name=xyz or /SayHello?name=xyz");
                 Console.WriteLine("Press <Enter> to stop the service.");
                 Console.ReadLine();
-                
-                
-                
-
+    
+    
+    
+    
                 // Close the ServiceHost.
                 host.Close();
-        
-            } catch (Exception ex) {
+    
+            }
+            catch (Exception ex)
+            {
                 Console.WriteLine($"*** Exception {ex.Message}");
                 host = null;
-        
-            } finally {
+    
+            }
+            finally
+            {
                 if (host != null) ((IDisposable)host).Dispose();
             }
-            
+    
         }
-        
-        
-        
-        
-        
-        /*
-        static public void Client()
-        {
-            
-            using (var wcf = new WebChannelFactory<INodeService>(new Uri("http://localhost:8081/hello"))) {
-                var channel = wcf.CreateChannel();
-    
-                
-                var names = Environment.GetCommandLineArgs();
-                
-                foreach (var name in names ) {
-                    
-    
-                    Console.WriteLine($"Calling SayHello for {name}");
-                    var res = channel.SayHello(name);
-                    Console.WriteLine($"... Response: {res}");
-                    Console.WriteLine("");
-                    
-                    Thread.Sleep (10);
-                }
-                
-                String name = "Enno";
-                Console.WriteLine($"Calling SayHello for {name}");
-                var res = channel.SayHello(name);
-                Console.WriteLine($"... Response: {res}");
-                Console.WriteLine("");
-            }
-        }    
-        */
-    
     }
-
+    
+    
     public class ClientClass
     {
         [ServiceContract()]
@@ -221,39 +207,33 @@ namespace learn
             string SayHello(string name);
         }
         
-        static public void ClientTo(int NodeNum)
+        static public void Client()
         {
-
-            String url = "http://localhost:"+Node.map[NodeNum.ToString()].ToString()+"/hello";
-            
-            var myChannelFactory = new WebChannelFactory<INodeService>(new Uri(url));
-            
-            var channel = myChannelFactory.CreateChannel();
-    
-            /*
-            var names = Environment.GetCommandLineArgs();
-            
-            foreach (var name in names ) {
                 
-
+                var myChannelFactory = new WebChannelFactory<INodeService>(new Uri("http://localhost:8082/hello"));
+         
+     
+                var channel = myChannelFactory.CreateChannel();
+    
+                /*
+                var names = Environment.GetCommandLineArgs();
+                
+                foreach (var name in names ) {
+                    
+    
+                    Console.WriteLine($"Calling SayHello for {name}");
+                    var res = channel.SayHello(name);
+                    Console.WriteLine($"... Response: {res}");
+                    Console.WriteLine("");
+                    
+                    Thread.Sleep (10);
+                }
+                */
+                String name = "Enno";
                 Console.WriteLine($"Calling SayHello for {name}");
                 var res = channel.SayHello(name);
                 Console.WriteLine($"... Response: {res}");
                 Console.WriteLine("");
-                
-                Thread.Sleep (10);
-            }
-            */
-                
-            String name = "Enno";
-            Console.WriteLine($"Calling SayHello for {name}");
-            var res = channel.SayHello(name);
-            Console.WriteLine($"... Response: {res}");
-            Console.WriteLine("");
-            
-            
-            Console.WriteLine($"This is Node {Node.ThisNode} sending message to {NodeNum}");
-            channel.Message(Node.ThisNode, NodeNum, 1,1);
             
         }
 
